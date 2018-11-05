7f4e762e823a797faeb42121faf07cece8c0b09f
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-05 19:15:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,7 +8,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist;
 }
 
-function removeFromPlaylist([playlist, artistName]) {
+function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist;
 }
