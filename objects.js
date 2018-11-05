77c0889bb7b5fe6565cc40f474738b3b6b3e43b4
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-05 18:02:56 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  Alison : "Slowdive";
+}
+function updatePlaylist(obj, key, value) {
+  obj[key] = value;
+}
+updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
+function removeFromPlaylist(obj, key) {
+  delete playlist[Phil Ochs];
+}
